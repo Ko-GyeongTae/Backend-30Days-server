f5e40ff6b6425e17d84bf46271fc1d655c4ca74b
Ko-GyeongTae <kokt0203@naver.com>
2021-04-26 09:12:31 +0900

    Add: Test Log

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private logger = new Logger();
   getHello(): string {
+    this.logger.log("[Log] Testing Server...");
     return 'Hello World!';
   }
 }
